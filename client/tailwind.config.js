d38852df85102c129882666471e6e74aadb82a03
ajummer <makeudreamempty@gmail.com>
2024-03-02 16:40:35 +0200

    Add screens to config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,30 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        primary: "#212e37",
+        secondary: "#ffff",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
     },
     screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
+      tablet: "640px",
+      laptop: "1024px",
+      desktop: "1280px",
     },
   },
   plugins: [],
-}
-
+};
