a6aabdf613a7b260070742587f246dc2d866ad11
ajummer <makeudreamempty@gmail.com>
2024-03-03 23:11:02 +0200

    Create AddMember component

---
+++ client/src/components/AddMember/AddMember.jsx
@@ -0,0 +1,9 @@
+import PersonAddAltRoundedIcon from "@mui/icons-material/PersonAddAltRounded";
+
+export default function AddMember() {
+  return (
+    <button className="btn btn-active btn-primary text-secondary btn-sm tablet:btn-md">
+      Добави член<PersonAddAltRoundedIcon />
+    </button>
+  );
+}
