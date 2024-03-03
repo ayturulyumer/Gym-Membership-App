c54f9b03f0d7ab2013bced90ce1249a94fbdf0dd
ajummer <makeudreamempty@gmail.com>
2024-03-03 15:35:35 +0200

    Update Search.jsx

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -2,6 +2,6 @@
 
 export default function Search() {
   return (
-    <input type="text" placeholder="Търсене" className="input input-bordered input-primary w-full max-w-xs" />
+    <input type="text" placeholder="Търсене" className="input input-sm tablet:input-md input-bordered input-primary w-full max-w-xs " />
   )
 }
