1d008fa7f04c930890ceee2120c011fb74761994
ajummer <makeudreamempty@gmail.com>
2024-03-03 15:37:10 +0200

    Update dependancies

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -13,7 +13,8 @@ module.exports = {
       },
     },
     screens: {
-      tablet: "640px",
+      phone: "640px",
+      tablet: "768px",
       laptop: "1024px",
       desktop: "1280px",
     },
