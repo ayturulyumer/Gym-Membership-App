f3c2473bed5655cba4c49dc90c76678c01c89209
ajummer <makeudreamempty@gmail.com>
2024-03-03 15:36:10 +0200

    Add Stats Component

---
+++ client/src/components/Stats/Stats.jsx
@@ -0,0 +1,32 @@
+import GroupsIcon from "@mui/icons-material/Groups";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import PersonOffIcon from '@mui/icons-material/PersonOff';
+
+export default function Stats() {
+  return (
+    <div className="stats shadow bg-primary text-secondary font-bold">
+      <div className="stat">
+        <div className="stat-figure text-primary">
+          <GroupsIcon htmlColor="white" />
+        </div>
+        <div className="stat-title text-success ">Общо членове</div>
+        <div className="stat-value text-secondary">5</div>
+      </div>
+
+      <div className="stat">
+        <div className="stat-figure">
+          <AccessTimeIcon  />
+        </div>
+        <div className="stat-title text-warning ">Изтичащи членства</div>
+        <div className="stat-value">2</div>
+      </div>
+      <div className="stat">
+        <div className="stat-figure">
+          <PersonOffIcon />
+        </div>
+        <div className="stat-title text-error ">Изтекли членства</div>
+        <div className="stat-value">1</div>
+      </div>
+    </div>
+  );
+}
