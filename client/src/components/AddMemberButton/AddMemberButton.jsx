0153ef62b5dad21018c2e1e1527049858060061e
ajummer <makeudreamempty@gmail.com>
2024-03-04 20:30:04 +0200

    Change addmember to addmember button

--- client/src/components/AddMemberButton/AddMemberButton.jsx
+++ client/src/components/AddMemberButton/AddMemberButton.jsx
@@ -1,6 +1,6 @@
 import PersonAddAltRoundedIcon from "@mui/icons-material/PersonAddAltRounded";
 
-export default function AddMember() {
+export default function AddMemberButton() {
   return (
     <button className="btn btn-active btn-primary text-secondary btn-sm tablet:btn-md">
       Добави член<PersonAddAltRoundedIcon />
