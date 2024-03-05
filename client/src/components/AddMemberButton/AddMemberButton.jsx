4f74cc719528aadc95c6f4530576991d9837b2d9
ajummer <makeudreamempty@gmail.com>
2024-03-05 18:40:52 +0200

    add toggle to open & close Modal

--- client/src/components/AddMemberButton/AddMemberButton.jsx
+++ client/src/components/AddMemberButton/AddMemberButton.jsx
@@ -1,9 +1,13 @@
 import PersonAddAltRoundedIcon from "@mui/icons-material/PersonAddAltRounded";
 
-export default function AddMemberButton() {
+export default function AddMemberButton({ showAddMemberHandler }) {
   return (
-    <button className="btn btn-active btn-primary text-secondary btn-sm tablet:btn-md">
-      Добави член<PersonAddAltRoundedIcon />
+    <button
+      onClick={showAddMemberHandler}
+      className="btn btn-active btn-primary text-secondary btn-sm tablet:btn-md"
+    >
+      Добави член
+      <PersonAddAltRoundedIcon />
     </button>
   );
 }
