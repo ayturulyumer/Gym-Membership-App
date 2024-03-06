053dffa12635c5ae330f50949fc735ca1b93e7e4
ajummer <makeudreamempty@gmail.com>
2024-03-06 17:37:25 +0200

    Add requester to form requests

---
+++ client/src/lib/request.js
@@ -0,0 +1,40 @@
+const buildOptions = (data) => {
+    const options = {
+      headers: {
+        Authorization:
+         `Bearer ${import.meta.env.VITE_APP_TOKEN}`
+      },
+    };
+  
+    if (data) {
+      options.body = JSON.stringify(data);
+      options.headers = {
+        ...options.headers,
+        "content-type": "application/json",
+      };
+    }
+  
+    return options;
+  };
+  
+  export const request = async (method, url, data) => {
+    const response = await fetch(url, {
+      ...buildOptions(data),
+  
+      method,
+    });
+  
+  
+    if (!response.ok) {
+      const result = await response.json();
+      throw result;
+    }
+    const result = await response.json();
+    return result;
+  };
+  
+  export const get = request.bind(null, "GET");
+  export const post = request.bind(null, "POST");
+  export const put = request.bind(null, "PUT");
+  export const del = request.bind(null, "DELETE");
+  export const patch = request.bind(null, "PATCH");
