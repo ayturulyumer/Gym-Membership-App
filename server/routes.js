4b464c98fada05693ff4696c64945c05b8784063
ajummer <makeudreamempty@gmail.com>
2024-03-06 12:26:40 +0200

    Add Routes

---
+++ server/routes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const adminController = require("./controllers/adminController.js");
+const membersController = require("./controllers/membersController.js");
+
+router.use("/admin", adminController);
+router.use("/members", membersController);
+
+module.exports = router;
