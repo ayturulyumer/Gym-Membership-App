df59ebfd3827d266ac6987fbeca55b517296b941
ajummer <makeudreamempty@gmail.com>
2024-03-06 12:44:40 +0200

    Add Admin Schema

---
+++ server/models/Admin.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+adminSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;
