710e8bb1156ac47915b5a3061f026d4942882b21
ajummer <makeudreamempty@gmail.com>
2024-03-08 23:18:43 +0200

    fetch data dynamically from server

--- client/src/components/SingleUser/SingleUser.jsx
+++ client/src/components/SingleUser/SingleUser.jsx
@@ -3,29 +3,33 @@ import AutorenewIcon from "@mui/icons-material/Autorenew";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-export default function SingleUser() {
+export default function SingleUser({ member }) {
+
   return (
     <tr className="text-secondary text-lg font-bold th-center">
-    <td>Hart Hagerty</td>
-    <td>Месечна</td>
-    <td>
-    <div className="badge badge-success text-secondary font-bold">Активен</div>
-    </td>
-    <td>3/2/2024</td>
-    <td>3/3/2024</td>
-    <td><div className="badge  badge-outline badge-success">30</div></td>
-    <td className="flex gap-2 justify-center">
-      <button className="tooltip" data-tip="Подновяване">
-        <AutorenewIcon htmlColor="green" />
-      </button>{" "}
-      <button className="tooltip" data-tip="Редактиране">
-        <EditIcon htmlColor="yellow" />
-      </button>{" "}
-      <button className="tooltip" data-tip="Изтриване">
-        <DeleteIcon htmlColor="red" />
-      </button>{" "}
-    </td>
-  </tr>
-  
+      <td>{member.name}</td>
+      <td>{member.cardType}</td>
+      <td>
+        <div className="badge badge-success text-secondary font-bold">
+          Активен
+        </div>
+      </td>
+      <td>{member.startDate}</td>
+      <td>{member.endDate}</td>
+      <td>
+        <div className="badge  badge-outline badge-success">30</div>
+      </td>
+      <td className="flex gap-2 justify-center">
+        <button className="tooltip" data-tip="Подновяване">
+          <AutorenewIcon htmlColor="green" />
+        </button>{" "}
+        <button className="tooltip" data-tip="Редактиране">
+          <EditIcon htmlColor="yellow" />
+        </button>{" "}
+        <button className="tooltip" data-tip="Изтриване">
+          <DeleteIcon htmlColor="red" />
+        </button>{" "}
+      </td>
+    </tr>
   );
 }
