4c58d0b909aa9c3c8be9364f857000f86e82f14a
ajummer <makeudreamempty@gmail.com>
2024-03-08 23:19:06 +0200

    Add Cyrillic converter

---
+++ client/src/utils/convertCardTypeToCyrillic.js
@@ -0,0 +1,14 @@
+export default function convertCardTypeToCyrillic(value) {
+  switch (value) {
+    case "monthly":
+      return "Месечна";
+    case "20workouts":
+      return "20 тренировки";
+    case "25workouts":
+      return "25 тренировки";
+    case "personalized":
+      return "Персонализиранa";
+    default:
+      return value;
+  }
+}
