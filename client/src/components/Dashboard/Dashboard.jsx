5ed276b6212dfc0afdf1b084b8361aeb8a50a604
ajummer <makeudreamempty@gmail.com>
2024-03-09 14:58:00 +0200

    fetch the members and set them in state
    I changed this operation to this component because i should also pass members to Stats Component

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,13 +1,24 @@
 import List from "../List/List.jsx";
 import Stats from "../Stats/Stats.jsx";
 import Nav from "../Nav/Nav.jsx";
+import { useState, useEffect } from "react";
+import * as membersApi from "../../api/membersApi.js";
 export default function Dashboard() {
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    membersApi
+      .getAllMembers()
+      .then((data) => setMembers(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <Nav />
       <main className="mx-auto my-12 w-11/12 min-h-[calc(100vh - 13rem)] flex flex-col gap-10">
-        <Stats />
-        <List />
+        <Stats members={members} />
+        <List members={members} />
       </main>
     </>
   );
