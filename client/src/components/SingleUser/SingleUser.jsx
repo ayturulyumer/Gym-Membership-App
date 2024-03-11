a4c1182138cc8dcc4096cafa9470bbee44d47640
ajummer <makeudreamempty@gmail.com>
2024-03-11 19:53:49 +0200

    Update SingleUser.jsx

--- client/src/components/SingleUser/SingleUser.jsx
+++ client/src/components/SingleUser/SingleUser.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import NoteIcon from "@mui/icons-material/Note";
 import AutorenewIcon from "@mui/icons-material/Autorenew";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import convertDateToBulgarian from "../../utils/convertDateToBulgarian.js";
 
-export default function SingleUser({ member }) {
+export default function SingleUser({
+  member,
+  showRenewModal,
+  onRenewModalClick,
+}) {
   // Get the current date & current member membership end date
   const currentDate = new Date();
   const userMembershipEndDate = member.endDate;
@@ -23,54 +28,73 @@ export default function SingleUser({ member }) {
   };
 
   return (
-    <tr className="text-secondary text-lg font-bold th-center">
-      <td className="badge text-secondary">{member.name}</td>
-      <td>{member.cardType}</td>
-      <td>
-        <div
-          className={
-            currentDateISO > userMembershipEndDate
-              ? "badge badge-error text-secondary font-bold"
-              : "badge badge-success text-primary font-bold"
-          }
-        >
-          {currentDateISO > userMembershipEndDate ? "Изтекъл" : "Активен"}
-        </div>
-      </td>
-      <td>
-        <span className="badge ">
-          {convertDateToBulgarian(member.startDate)}
-        </span>
-      </td>
-      <td>
-        <span className="badge ">{convertDateToBulgarian(member.endDate)}</span>
-      </td>
-      <td>
-        <div className="badge badge-default text-secondary">
-          {member.cardType === "Месечна" ||
-          member.cardType === "Персонализиранa"
-            ? calculateRemainingDays() + " дни"
-            : member.cardType === "20 тренировки" ||
-              member.cardType === "25 тренировки"
-            ? calculateRemainingDays() +
-              " дни" +
-              " / " +
-              member.workouts +
-              " тренировки"
-            : ""}
-        </div>
-      </td>
-      <td className="flex gap-2 justify-center">
-        <button className="tooltip" data-tip="Подновяване">
-          <AutorenewIcon htmlColor="green" />
-        </button>{" "}
-        <button className="tooltip" data-tip="Редактиране">
-          <EditIcon htmlColor="yellow" />
-        </button>{" "}
-        <button className="tooltip" data-tip="Изтриване">
-          <DeleteIcon htmlColor="red" />
-        </button>{" "}
-      </td>
-    </tr>
+    <>
+      <tr className="text-secondary text-lg font-bold th-center">
+        <td className="badge text-secondary">{member.name}</td>
+        <td>{member.cardType}</td>
+        <td>
+          <div
+            className={
+              currentDateISO > userMembershipEndDate
+                ? "badge badge-error text-secondary font-bold"
+                : "badge badge-success text-secondary font-bold"
+            }
+          >
+            {currentDateISO > userMembershipEndDate ? "Изтекъл" : "Активен"}
+          </div>
+        </td>
+        <td>
+          <span className="badge ">
+            {convertDateToBulgarian(member.startDate)}
+          </span>
+        </td>
+        <td>
+          <span className="badge ">
+            {convertDateToBulgarian(member.endDate)}
+          </span>
+        </td>
+        <td>
+          <div className="badge badge-default text-secondary">
+            {(() => {
+              const remainingDays = calculateRemainingDays();
+              if (remainingDays === 1) {
+                return (
+                  <>
+                    {remainingDays} ден
+                    {member.cardType.includes("тренировки") &&
+                      ` / ${member.workouts} тренировки`}
+                  </>
+                );
+              } else if (remainingDays > 1) {
+                return (
+                  <>
+                    {remainingDays} дни
+                    {member.cardType.includes("тренировки") &&
+                      ` / ${member.workouts} тренировки`}
+                  </>
+                );
+              } else {
+                return ""; // Return empty string if remainingDays is less than 1
+              }
+            })()}
+          </div>
+        </td>
+        <td className="flex gap-2 justify-center">
+          <button
+            className="tooltip"
+            data-tip="Подновяване"
+            onClick={onRenewModalClick}
+          >
+            <AutorenewIcon htmlColor="green" />
+          </button>{" "}
+          <button className="tooltip" data-tip="Редактиране">
+            <EditIcon htmlColor="yellow" />
+          </button>{" "}
+          <button className="tooltip" data-tip="Изтриване">
+            <DeleteIcon htmlColor="red" />
+          </button>{" "}
+        </td>
+      </tr>
+    </>
   );
 }
