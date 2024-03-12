7795c88bf5e76bff4469e7dedd9e375a8f6f502b
ajummer <makeudreamempty@gmail.com>
2024-03-12 12:51:20 +0200

    await the login request

--- client/src/api/adminApi.js
+++ client/src/api/adminApi.js
@@ -2,10 +2,10 @@ const baseUrl = import.meta.env.VITE_APP_BASEURL;
 import * as request from "../lib/request.js";
 
 export const login = async (data) => {
-  const result = request.post(`${baseUrl}/admin/login`, data);
+  const result = await request.post(`${baseUrl}/admin/login`, data);
   return result;
 };
 
 export const logout = async () => {
- request.get(`${baseUrl}/admin/logout`);
+  request.get(`${baseUrl}/admin/logout`);
 };
