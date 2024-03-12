78dead9f6d39f956f59f3e3cfdc432b0c2020d84
ajummer <makeudreamempty@gmail.com>
2024-03-12 12:52:17 +0200

    Add update and delete member controller

--- server/controllers/membersController.js
+++ server/controllers/membersController.js
@@ -24,4 +24,29 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:memberId", async (req, res) => {
+  try {
+    const updatedMember = await membersManager.update(
+      req.params.memberId,
+      req.body
+    );
+    res.status(200).json(updatedMember);
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
+router.delete("/:memberId", async (req, res) => {
+  try {
+    await membersManager.delete(req.params.memberId);
+    res.status(200).json("Successfully deleted");
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
 module.exports = router;
