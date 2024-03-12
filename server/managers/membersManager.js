8f66539670486bd8e2306215e18aefa34650d82c
ajummer <makeudreamempty@gmail.com>
2024-03-12 12:52:44 +0200

    Add update and delete manager

--- server/managers/membersManager.js
+++ server/managers/membersManager.js
@@ -3,3 +3,10 @@ const Member = require("../models/Member.js");
 exports.create = (memberData) => Member.create(memberData);
 
 exports.getAll = () => Member.find();
+
+exports.update = (memberId, memberData) => {
+  const updatedMember = Member.findByIdAndUpdate(memberId, memberData);
+  return updatedMember;
+};
+
+exports.delete = (memberId) => Member.findByIdAndDelete(memberId);
