064bc80db223b7c0be4b42fa5750e780d0b70803
ajummer <makeudreamempty@gmail.com>
2024-03-13 12:52:12 +0200

    Add search function

--- server/managers/membersManager.js
+++ server/managers/membersManager.js
@@ -4,8 +4,22 @@ exports.create = (memberData) => Member.create(memberData);
 
 exports.getAll = () => Member.find();
 
-exports.update = (memberId, memberData) => Member.findByIdAndUpdate(memberId, memberData, { new: true });
-
-
+exports.update = (memberId, memberData) =>
+  Member.findByIdAndUpdate(memberId, memberData, { new: true });
 
 exports.delete = (memberId) => Member.findByIdAndDelete(memberId);
+
+exports.decreaseWorkout = async (memberId) => {
+  const member = await Member.findById(memberId);
+  if (member.workouts != 0) {
+    member.workouts -= 1;
+  }
+  await member.save();
+  return member;
+};
+
+exports.search = (searchCriteria) => {
+  if (searchCriteria) {
+    return Member.find({ name: searchCriteria }).lean();
+  }
+};
