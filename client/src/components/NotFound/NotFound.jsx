780367c63c56bd461f78dbb43d9b0c052403946f
ajummer <makeudreamempty@gmail.com>
2024-03-17 12:39:37 +0200

    Change css

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -2,20 +2,20 @@ import { useNavigate } from "react-router-dom";
 export default function NotFound() {
   const navigate = useNavigate();
   return (
-    <main class="h-screen w-full flex flex-col justify-center items-center bg-primary">
-      <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
-      <div class="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
-        Page Not Found
+    <main className="h-screen w-full flex flex-col justify-center items-center bg-primary">
+      <h1 className="text-9xl font-extrabold text-secondary tracking-widest">404</h1>
+      <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute text-primary">
+        Страницата не е намерена
       </div>
-      <button class="mt-5">
-        <a class="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
-          <span class="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
+      <button className="mt-5">
+        <a className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
+          <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
           <span
-            class="relative block px-8 py-3 bg-[#1A2238] border border-current"
+            className="relative block px-8 py-3 bg-[#1A2238] border border-current"
             onClick={() => navigate(-1)}
           >
-            Върни назад
+           Назад
           </span>
         </a>
       </button>
