f79a29472c076bd1e32600cc5131bccd77570c04
ajummer <makeudreamempty@gmail.com>
2024-03-17 12:00:26 +0200

    Add callback to get the current sorting option

--- client/src/components/Sort/Sort.jsx
+++ client/src/components/Sort/Sort.jsx
@@ -1,12 +1,21 @@
-export default function Sort() {
+import { useState } from "react";
+export default function Sort({ onSortChange }) {
+  const [selectedOption, setSelectedOption] = useState("default");
+
+  const handleSortChange = (event) => {
+    const selectedValue = event.target.value;
+    setSelectedOption(selectedValue);
+    onSortChange(selectedValue); // Pass the selected sorting option to the parent component
+  };
   return (
     <select
       className="select select-primary w-full max-w-xs select-sm tablet:select-md"
-      defaultValue="default"
+      value={selectedOption}
+      onChange={handleSortChange}
     >
       <option value="default">Сортиране по подразбиране</option>
-      <option value="name">Име</option>
-      <option value="status">Статус</option>
+      <option value="expiringMemberships">Изтичащи членства</option>
+      <option value="expiredMemberships">Изтекли членства</option>
       <option value="remainingWorkouts">Oставащи тренировки</option>
     </select>
   );
