d9e1b38623957956daf744d57269af39c107d360
ajummer <makeudreamempty@gmail.com>
2024-03-19 12:13:21 +0200

    Remove nav from dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 import List from "../List/List.jsx";
 import Stats from "../Stats/Stats.jsx";
-import Nav from "../Nav/Nav.jsx";
 import { useState, useEffect } from "react";
 import * as membersApi from "../../api/membersApi.js";
 import InfoMessage from "../InfoMessage/InfoMessage.jsx";
@@ -87,7 +86,6 @@ export default function Dashboard() {
   };
   return (
     <>
-      <Nav />
       <main className="mx-auto my-12 w-11/12 min-h-[calc(100vh - 13rem)] flex flex-col gap-10">
         <Stats members={members} />
         {message === "error" ? (
