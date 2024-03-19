4d5f7022c04586f1eb740027d83a206f87d6b0d8
ajummer <makeudreamempty@gmail.com>
2024-03-19 13:00:48 +0200

    Change username with email & Error messages

--- server/managers/adminManager.js
+++ server/managers/adminManager.js
@@ -3,29 +3,29 @@ const bcrypt = require("bcrypt");
 const jwt = require("../lib/jwt.js");
 const secretKey = process.env.SECRET_KEY;
 
-exports.login = async ({ username, password }) => {
-  const user = await Admin.findOne({ username });
+exports.login = async ({ email, password }) => {
+  const user = await Admin.findOne({ email });
 
   if (!user) {
-    throw new Error("Invalid username or password !");
+    throw new Error("Грешен имейл или парола");
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
 
   if (!isPasswordValid) {
-    throw new Error("Invalid username or password !");
+    throw new Error("Грешен имейл или парола!");
   }
 
   const payload = {
     _id: user._id,
-    username: user.username,
+    email: user.email,
   };
 
   const token = await jwt.sign(payload, secretKey, { expiresIn: "2d" });
 
   const result = {
     _id: user._id,
-    username: user.username,
+    email: user.email,
     accessToken: token,
   };
 
