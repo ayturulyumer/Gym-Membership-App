254de87aeb0d5adf60cb93aa69532db8c43effd9
ajummer <makeudreamempty@gmail.com>
2024-03-25 16:30:19 +0200

    remove dummy data

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -2,15 +2,6 @@ const router = require("express").Router();
 
 const adminManager = require("../managers/adminManager.js");
 
-router.get("/", async (req, res) => {
-  try {
-    res.status(200).json("BRATAANNNNNN");
-  } catch (error) {
-    res.status(400).json({
-      message: error.message,
-    });
-  }
-});
 
 router.post("/login", async (req, res) => {
   try {
