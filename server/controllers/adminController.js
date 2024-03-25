e563ddb68c42500b721bdfbf902ffc04488c963d
ajummer <makeudreamempty@gmail.com>
2024-03-25 14:56:10 +0200

    Update adminController.js

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -2,6 +2,16 @@ const router = require("express").Router();
 
 const adminManager = require("../managers/adminManager.js");
 
+router.get("/", async (req, res) => {
+  try {
+    res.status(200).json("BRATAANNNNNN");
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
 router.post("/login", async (req, res) => {
   try {
     const result = await adminManager.login(req.body);
